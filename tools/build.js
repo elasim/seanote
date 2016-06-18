d8d9c49a1a6d9468e1e25b08e5d1c04efd55f25b
lequntus <lequntus@pc101.local>
2016-06-18 09:57:38 +0900

    add makePackage() on build script

--- tools/build.js
+++ tools/build.js
@@ -1,4 +1,6 @@
+import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 import webpack from 'webpack';
 import './common/setup';
 
@@ -8,7 +10,8 @@ const serverCompiler = webpack(config.server);
 
 Promise.all([
 	runCompiler(clientCompiler),
-	runCompiler(serverCompiler)
+	runCompiler(serverCompiler),
+	makePackage(),
 ])
 	.then(() => {
 		console.log('Build Success');
@@ -25,6 +28,28 @@ function runCompiler(compiler) {
 				colors: true,
 				chunks: global.VERBOSE
 			}));
+			e ? reject(e) : resolve();
+		});
+	});
+}
+
+function makePackage() {
+	const ejectFields = [
+		'devDependencies',
+		'scripts'
+	];
+	const overwriteFields = {
+		'main': './server.js'
+	};
+	const inputPath = path.join(process.cwd(), './package.json');
+	const outputPath = path.join(config.server.output.path, './package.json');
+	let pkg = _.cloneDeep(require(inputPath));
+	ejectFields.forEach(field => {
+		delete pkg[field];
+	});
+	Object.assign(pkg, overwriteFields);
+	return new Promise((resolve, reject) => {
+		fs.writeFile(outputPath, JSON.stringify(pkg, null, 4), (e) => {
 			e ? reject(e) : resolve();
 		});
 	});
