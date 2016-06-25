7b837dfe8928017031e46e22d82ffbdbef934373
Lee JeongHo <lequntus@hotmail.com>
2016-06-26 07:23:11 +0900

    add missing redux-thunk middleware for production

--- src/common/store/production.js
+++ src/common/store/production.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
 export default function configureStore(initialState) {
 	return createStore(
 		reducer,
 		initialState,
-		// applyMiddleware(
-		// 	thunk()
-		// )
+		applyMiddleware(
+			thunk
+		)
 	);
 }
