23dbc416bff0a069eb18f5066d0ed5f46b3b633b
Lee JeongHo <lequntus@hotmail.com>
2016-07-09 19:38:47 +0900

    Fix(api): optimize query for board list

--- src/server/routes/api/board-list.js
+++ src/server/routes/api/board-list.js
@@ -3,19 +3,13 @@ import { User, Board } from '../../data';
 
 export default {
 	get: [requireAuth, async (req) => {
-		console.log(req.user);
 		const user = await User.findById(req.user.claim.aud);
-		const author = await user.getAuthor();
-		// Constraints Corruption
-		if (!author) {
-			throw new Error('Cannot found author key for user');
-		}
 		const paging = {
 			limit: Math.max(1, Math.min(100, req.query.limit || 10)),
 			offset: Math.max(req.query.offset || 0, 0)
 		};
 		const where = {
-			OwnerId: author.id
+			OwnerId: user.AuthorId
 		};
 		const items = await Board.findAll({
 			where,
