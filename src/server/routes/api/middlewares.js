8b5cf8fed09f3b53c37572d0f89d48cbee016c66
Lee JeongHo <lequntus@hotmail.com>
2016-07-14 00:13:19 +0900

    requireAuth will accepts token

--- src/server/routes/api/middlewares.js
+++ src/server/routes/api/middlewares.js
@@ -1,7 +1,23 @@
+import { verifyToken } from '../../lib/session';
+import { User } from '../../data';
 
-export function requireAuth(req, res, next) {
-	if (!req.user && !req.query.token) {
+export async function requireAuth(req, res, next) {
+	if (req.user) {
+		if (req.query.token) {
+			return next(new Error('You are already logged, but token was served'));
+		}
+		return next();
+	} else if (req.query.token) {
+		try {
+			const claim = await verifyToken(req.query.token);
+			const db = await User.findById(claim.aud);
+			req.user = { claim, db };
+			next();
+		} catch (e) {
+			console.error('api token failure', e);
+			return next(new Error('invalid token'));
+		}
+	} else {
 		return next(new Error('authentication required'));
 	}
-	next();
 }
