e43fdf94554f8c59d9f7948bf40086417cd4a8e3
Lee JeongHo <lequntus@hotmail.com>
2016-07-13 23:56:01 +0900

    refactor

--- src/common/data/user.js
+++ src/common/data/user.js
@@ -1,12 +1,14 @@
 import request from '../lib/request';
 
 export default {
-	async whoami() {
-		try {
-			const res = await request.get('/api/user');
-			return await res.json();
-		} catch (e) {
-			return null;
-		}
-	},
+	whoami,
 };
+
+export async function whoami() {
+	try {
+		const res = await request.get('/api/user');
+		return await res.json();
+	} catch (e) {
+		return null;
+	}
+}
