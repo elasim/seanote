477623fdc21d288e8cf52b08c167ddb12b9d3e3c
Lee JeongHo <lequntus@hotmail.com>
2016-07-13 23:55:42 +0900

    add symbol polyfill

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ import 'core-js/fn/array/find-index';
 import 'core-js/fn/array/fill';
 import 'core-js/fn/object/assign';
 import 'core-js/fn/promise';
+import 'core-js/es6/symbol';
 import 'whatwg-fetch';
 
 import './bootstrap';
